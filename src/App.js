b2cb0ac71b0b72fe606220049ffca3a083097607
Taesun Lee <superbsun@gmail.com>
2020-04-08 20:17:41 +0900

    clean App class

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    count: 0,
-  };
-
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-
-  // it is called after render()
-  componentDidMount(props) {
-    console.log("component renders");
-  }
-
-  componentDidUpdate(props) {
-    console.log("i just updated");
-  }
-
-  componentWillUnmount(props) {
-    console.log("Goodbye, cruel world");
-  }
-
   render() {
-    console.log("I'm rendering");
-
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    return <div></div>;
   }
 }
 
