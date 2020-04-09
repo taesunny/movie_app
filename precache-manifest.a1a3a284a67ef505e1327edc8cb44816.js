925887004020f2b4d86e1b99445e5455f3e68d69
Taesun Lee <superbsun@gmail.com>
2020-04-09 22:55:07 +0900

    Updates

---
+++ precache-manifest.a1a3a284a67ef505e1327edc8cb44816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678a20827b38819b4d45b45fd903c565",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "aeb7178b5159e1226dd2",
+    "url": "/movie_app/static/css/main.2820df7a.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb7178b5159e1226dd2",
+    "url": "/movie_app/static/js/main.fa969e08.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
